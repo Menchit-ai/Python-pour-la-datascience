6f587f6d375d88e90944854982fb658af00a3289
Lilian De Conti <lilian.deconti@edu.esiee.fr>
2020-09-21 15:53:47 +0200

    Reste à ajouter la data df pour la carte

---
+++ carte.py
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+#function to convert to alpha2 country codes and continents
+from pycountry_convert import country_alpha2_to_continent_code, country_name_to_country_alpha2
+def get_continent(col):
+    try:
+        cn_a2_code =  country_name_to_country_alpha2(col)
+    except:
+        cn_a2_code = 'Unknown' 
+    try:
+        cn_continent = country_alpha2_to_continent_code(cn_a2_code)
+    except:
+        cn_continent = 'Unknown' 
+    return (cn_a2_code, cn_continent)
+
+#function to get longitude and latitude data from country name
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent = "appli")
+def geolocate(country):
+    try:
+        # Geolocate the center of the country
+        loc = geolocator.geocode(country)
+        # And return latitude and longitude
+        return (loc.latitude, loc.longitude)
+    except:
+        # Return missing value
+        return np.nan
+
+# Create a world map to show distributions of users 
+import folium
+from folium.plugins import MarkerCluster
+#empty map
+world_map= folium.Map(tiles="cartodbpositron")
+marker_cluster = MarkerCluster().add_to(world_map)
+#for each coordinate, create circlemarker of user percent
+for i in range(len(df)):
+        lat = df.iloc[i]['Latitude']
+        longit = df.iloc[i]['Longitude']
+        radius=5
+        popup_text = """Country : {}<br>
+                    %of Users : {}<br>"""
+        popup_text = popup_text.format(df.iloc[i]['Country'],
+                                   df.iloc[i]['User_Percent']
+                                   )
+        folium.CircleMarker(location = [lat, longit], radius=radius, popup= popup_text, fill =True).add_to(marker_cluster)
+#show the map
+world_map
--- dashboard.py
+++ dashboard.py
@@ -28,6 +28,18 @@ if __name__ == '__main__':
 
     app = dash.Dash(__name__) # (3)
 
+    #@app.callback(
+    #Output(component_id='graph1', component_property='figure'), # (1)
+    #[Input(component_id='year-dropdown', component_property='value')] # (2)
+    #)
+    #Output et Input marche pas
+    def update_figure(input_value): # (3)
+        return px.scatter(data[input_value], x="gdpPercap", y="lifeExp",
+                        color="continent",
+                        size="pop",
+                        hover_name="country") # (4)
+
+
     fig = px.scatter(data[year], x="gdpPercap", y="lifeExp",
                         color="continent",
                         size="pop",
@@ -43,6 +55,26 @@ if __name__ == '__main__':
                                 id='graph1',
                                 figure=fig
                             ), # (6)
+
+                            html.Label('Year'),
+                            dcc.Dropdown(
+                                id="year-dropdown",
+                                options=[
+                                    {'label': '1952', 'value': 1952},
+                                    {'label': '1957', 'value': 1957},
+                                    {'label': '1962', 'value': 1962},
+                                    {'label': '1967', 'value': 1967},
+                                    {'label': '1972', 'value': 1972},
+                                    {'label': '1977', 'value': 1977},
+                                    {'label': '1982', 'value': 1982},
+                                    {'label': '1987', 'value': 1987},
+                                    {'label': '1992', 'value': 1992},
+                                    {'label': '1997', 'value': 1997},
+                                    {'label': '2002', 'value': 2002},
+                                    {'label': '2007', 'value': 2007},
+                                ],
+                                value=2007,
+                            ),
 
                             html.Div(children=f'''
                                 The graph above shows relationship between life expectancy and
