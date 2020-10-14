53720e57d55406584c439f898d51b7c2d027e456
Lilian De Conti <lilian.deconti@edu.esiee.fr>
2020-10-14 12:56:02 +0200

    Il faut juste améliorer la description du slider

--- createDatapack.py
+++ createDatapack.py
@@ -11,6 +11,11 @@ import pycountry
 import numpy as np
 import datetime
 
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
 # dataAbo = pd.read_csv("abo.csv",delimiter=",")
 
 # # passer TIME en date en supprimant les semestres
@@ -127,6 +132,61 @@ def standardizeData(liData,model="basic"):
 
     return stdData
 
+###############################################################################################################    
+
+def uniqueYear(dataf, yearname):
+    return dataf[yearname].unique()
+
+def createDataDic(dataf, yearname, years):
+    return {year:dataf.query(yearname + " == @year") for year in years}
+
+def createFig(data_dic, yearnumber, value1, value2, value3, value4):
+
+    fig = px.scatter(data_dic[yearnumber], x=data_dic[yearnumber][value1], y=data_dic[yearnumber][value2], #data[input_value]
+                        color=data_dic[yearnumber][value3], #size="pop",
+                        hover_name=data_dic[yearnumber][value4])
+
+    return fig
+
+def dashBoard(dataf, years, fig, value1, value2, value3, value4, appl):
+
+    appl.layout = html.Div([
+        dcc.Tabs([
+            dcc.Tab(label='Graphe', children=[
+                html.H1(children=f'Titre',   #{year}
+                                style={'textAlign': 'center', 'color': '#7FDBFF'}), # (5)
+
+                dcc.Graph(
+                    id='graph1',
+                    figure=fig
+                ), # (6)
+
+                html.Label('Year'),
+                dcc.Slider(
+                    id="year-slider",
+                    min = years[0],
+                    max = years[len(years)-1],
+                    step = 1,
+                    #marks=[{year : str(year) for year in years}],
+                    value=years[0]
+                )
+            ]),
+        ])
+    ])
+
+    @appl.callback(
+    Output(component_id='graph1', component_property='figure'), # (1)
+    [Input(component_id='year-slider', component_property='value')] # (2)
+    )
+
+    def update_figure(input_value): # (3)
+        return px.scatter(dataf[input_value], x=dataf[input_value][value1], y=dataf[input_value][value2], #data[input_value]
+                        color=dataf[input_value][value3], #size="pop",
+                        hover_name=dataf[input_value][value4]) # (4)
+
+
+###############################################################################################################
+
 
 if __name__ == "__main__":
     
@@ -148,6 +208,22 @@ if __name__ == "__main__":
     l = standardizeData([data])
     data = l[0]
     print("colonnes standardes :"+ str(data.columns))
+
+    nTab = len(data.columns)
+    #print(data.head())
     # graph.show()
     # plotly.offline.plot(graph, filename='fig.html', auto_open=True, include_plotlyjs='cdn')
 # https://plotly.com/python/text-and-annotations/
+
+    diffyears = uniqueYear(data, 'Year')
+    diffyears.sort()
+    dico = createDataDic(data, 'Year', diffyears)
+
+    print(diffyears)
+    print(dico[2008])
+    fig = createFig(dico, 2008, str(data.columns[nTab-1]), str(data.columns[nTab-2]), 'Code', 'Entity')
+
+
+    app = dash.Dash(__name__)
+    dashBoard(dico, diffyears, fig, str(data.columns[nTab-1]), str(data.columns[nTab-2]), 'Code', 'Entity', app)
+    app.run_server(debug=True)
